ae8921348f9ee1401da5eb293596864de0d33a48
DeeplyBrinedCode <135945715+DeeplyBrinedCode@users.noreply.github.com>
2025-12-11 03:38:16 -0800

    Format and clean up polar chart setup code

--- rpi/polarchart.py
+++ rpi/polarchart.py
@@ -2,7 +2,6 @@ import matplotlib
 matplotlib.use("TkAgg")
 
 import matplotlib.pyplot as plt
-import numpy as np
 import time
 
 # Polar radar plot setup
@@ -20,5 +19,6 @@ plt.pause(0.1)
 
 fig.canvas.draw()
 fig.canvas.flush_events()
+
 
 
